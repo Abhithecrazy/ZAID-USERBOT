ffd2c3927b940e124d19ff87e15f0c1bbe401156
Abhithecrazy <114983925+Abhithecrazy@users.noreply.github.com>
2022-12-10 13:27:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ from os import getenv
 API_ID = int(getenv("API_ID", "6435225")) #optional
 API_HASH = getenv("API_HASH", "") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
-OWNER_ID = int(getenv("OWNER_ID", ""))
-MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC")
-ALIVE_TEXT = getenv("ALIVE_TEXT")
-PM_LOGGER = getenv("PM_LOGGER")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "2059629534").split()))
+OWNER_ID = int(getenv("OWNER_ID", "5739473948"))
+MONGO_URL = getenv("MONGO_URL", 'mongodb+srv://amit:amit8894@cluster0.7zcfz.mongodb.net/?retryWrites=true&w=majority')
+BOT_TOKEN = getenv("BOT_TOKEN", "5949721257:AAGlkfbYiOXpY5dYw4JpXnkb5hnDdGuQL70")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://te.legra.ph/file/6e84f2c2eb6955161e401.jpg')
+ALIVE_TEXT = getenv("ALIVE_TEXT", '𝙠𝙖𝙖𝙢 𝙗𝙤𝙡 𝘽𝙖𝙠𝙞𝙘𝙝𝙤𝙙𝙞 𝙣𝙚 𝙠𝙖𝙧𝙖 𝙨𝙪𝙖𝙧 𝙠𝙞 𝙜𝙖𝙣𝙙')
+PM_LOGGER = getenv("PM_LOGGER", '-')
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
 BRANCH = getenv("BRANCH", "master") #don't change
  
-STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION1 = getenv("STRING_SESSION1", "BQA9vpjfyLM8s2yq0HXniR8QwGBcKXM5h8Ud8yAfqe9AR-MV1NKL-rmAPNT6M5VJ4q2djofiKkrY-rzFE2uL5-ZOPGPXrsdxL6FUKF7oNjIAsG6G0gw3w2WZ1OwFGj6JGSRMSESeHw7voTc0NZdFSWom3PW471Y3UcQqen4zB19wQZaGcJsQYzE3Qf2iznzfhBVrgAwrT2v_oyEw6xAoAcfxpczlCkOolgv8XwaIMQatP8XRK7Tq5rvjwVIAp0TaUUiAbCDepQnwOAJ08QhyLLFyHRtWmqPctIAht-Pyee4PdTn5_9C6Sj0x6dy-TpAqYjWNmrtqoinggA7e_MPe_h75AAAAAVYZbBwA")
 STRING_SESSION2 = getenv("STRING_SESSION2", "")
 STRING_SESSION3 = getenv("STRING_SESSION3", "")
 STRING_SESSION4 = getenv("STRING_SESSION4", "")
